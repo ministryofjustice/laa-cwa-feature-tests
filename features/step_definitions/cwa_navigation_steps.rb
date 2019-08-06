6b11d44f2afcaea333765954af4e141c5affa4d3
Giuseppe Lobraico <giuseppe.lobraico@digital.justice.gov.uk>
2019-08-06 16:45:00 +0100

    Remove sleep
    There seems to be a callback somewhere that resets the submission
    period field.
    
    This sleep is a leftover, as removing the `fill_in` of the office name
    with a previous commit on this branch seems to suffice to avoid the reset.

--- features/step_definitions/cwa_navigation_steps.rb
+++ features/step_definitions/cwa_navigation_steps.rb
@@ -33,7 +33,6 @@ When('user searches for their provider submission') do
   fill_in 'SearchFirmName', with: CWAProvider.firm_name
   fill_in 'SearchLscAccountNo', with: CWAProvider.account_number
   page.select CWAProvider.legal_help_submission.area_of_law, from: 'AreaOfLawSearch'
-  sleep(0.75)
   fill_in 'SearchSubmissionPeriod', with: CWAProvider.legal_help_submission.period
   click_button 'Go'
   expect(page).to have_content(CWAProvider.legal_help_submission.reference)
