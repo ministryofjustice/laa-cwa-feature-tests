3951d3e987db7c6c338d7bb6ee484d1147c46aa2
deepak.tanna <deepak.tanna@digital.justice.gov.uk>
2021-05-19 16:29:19 +0100

    TA-1107 updated files to ensure tests are working

--- features/support/helpers/page.rb
+++ features/support/helpers/page.rb
@@ -6,7 +6,7 @@ module Helpers
       popup_window = window_opened_by { trigger.call }
       original_window = original_page.current_window
       within_window(popup_window) { yield }
-      switch_to_window(original_window)
+    #  switch_to_window(original_window)
     end
   end
 end
