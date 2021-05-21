913b37507e4f99aecd99f697ba40f61a8fd08c9a
deepak.tanna <deepak.tanna@digital.justice.gov.uk>
2021-05-21 16:06:59 +0100

    removed lines not required in page.rb

--- features/support/helpers/page.rb
+++ features/support/helpers/page.rb
@@ -4,9 +4,7 @@ module Helpers
       return unless block_given?
 
       popup_window = window_opened_by { trigger.call }
-      original_window = original_page.current_window
       within_window(popup_window) { yield }
-    #  switch_to_window(original_window)
     end
   end
 end
