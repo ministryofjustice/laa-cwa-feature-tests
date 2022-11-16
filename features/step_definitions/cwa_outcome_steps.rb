029edcbabf05684f068a1ec0cede915c0b274519
Solomon Ahmed <51327770+solomonahmed@users.noreply.github.com>
2022-11-16 20:37:32 +0000

    refactor: refactored 1 existing step definition (sd) and replaced some tightly coupled sd's with a more flexible one
    A decision was made to re-factor some sd's in cwa_outcome_steps to support a new feature.
    This meant making an existing sd more flexible by allowing it to be called with an area of law
    and category of law.  This info is used to retrieve the correct set of fields that need to be populated
    together with any defaults.  This sd expects a data table from the feature tests.  It loops round
    creating records based defaults/fields given the passed area of law/category of law and allowing the
    data table to supply default value overrides.
    
    In addition, there were some legacy sd's which were quite large and heavily tied to certain Legal Help
    categories of law, as the name of the category of law was included in the sd name.  These have all
    been replaced by a more flexible version.  This sd is called from feature tests using an example tables.
    There is still some coupling as the more flexible replacement sd still expects a certain number of fixed fields.
    But it can be passed an area of law and category of law (similar to the data table sd) and uses this to
    grab the correct set of fields/defaults to use (overriding a fixed number of values).
    
    For entering submissions into CWA from a feature test there are now 2 main approaches that can be taken.
    Either the data table approach or the example table approach.  The sd's which implement them both make use
    of the yaml files to retrieve fields/default values which are then overridden by values passed from the feature test.

--- features/step_definitions/cwa_outcome_steps.rb
+++ features/step_definitions/cwa_outcome_steps.rb
@@ -15,10 +15,15 @@ end
 
 Then('the outcome saves successfully') do
   page = AddOutcomePage.new
-  expect(page.has_no_errors?(wait: 0)).to eq(true)
+  begin
+    expect(page.has_no_errors?(wait: 0)).to eq(true)
+  rescue RSpec::Expectations::ExpectationNotMetError
+    STDOUT.puts page.errors.text
+    raise RSpec::Expectations::ExpectationNotMetError
+  end
 end
 
-When("user adds outcomes for Immigration with fields like this:") do |table|
+When("user adds outcomes for {string} {string} with fields like this:") do |area_of_law, category_of_law, table|
   sr = CWAProvider.submission.schedule_ref
   outcome_data = table.hashes
   @submissions_saved = outcome_data.size
@@ -27,245 +32,42 @@ When("user adds outcomes for Immigration with fields like this:") do |table|
     submission_list_page.add_outcome_button.click
     test_data_row["schedule_ref"] = sr
     builder = Helpers::ScreenFieldBuilder.from(
-      area_of_law: 'legal_help',
-      category_of_law: 'immigration')
+      area_of_law: area_of_law.downcase.gsub(' ', '_'),
+      category_of_law: category_of_law.downcase.gsub(' ', '_')
+    )
     builder.overrides = test_data_row
     page = AddOutcomePage.new(builder)
     page.add_outcome
   end
 end
 
-When("user adds outcomes for Mental Health with fields like this:") do |table|
-  sr = CWAProvider.submission.schedule_ref
-  outcome_data = table.hashes
-  @submissions_saved = outcome_data.size
-  outcome_data.each do |test_data_row|
+When ('user adds an outcome for {string} {string} with {string}, {string}, {string}, {string}, {string}, {string} and {string}') \
+  do |area_of_law, category_of_law, case_id, matter_type, ecs, ecf_ref, case_start_date, pa, ap |
+    sr = CWAProvider.submission.schedule_ref
+
+    outcome_data = Hash.new
+      
+    outcome_data["matter_type"] = matter_type
+    outcome_data["excl_case_funding_ref"] = ecf_ref
+    outcome_data["case_start_date"] = case_start_date
+    outcome_data["procurement_area"] = pa
+    outcome_data["access_point"] = ap
+    outcome_data["case_id"] = case_id
+    outcome_data["exemption_criteria_satisfied"] = ecs
+
     submission_list_page = SubmissionListPage.new
     submission_list_page.add_outcome_button.click
-    test_data_row["schedule_ref"] = sr
+    
+    outcome_data["schedule_ref"] = sr
     builder = Helpers::ScreenFieldBuilder.from(
-      area_of_law: 'legal_help',
-      category_of_law: 'mental_health')
-    builder.overrides = test_data_row
+      area_of_law: area_of_law.downcase.gsub(' ', '_'),
+      category_of_law: category_of_law.downcase.gsub(' ', '_')
+    )
+
+    builder.overrides = outcome_data
     page = AddOutcomePage.new(builder)
     page.add_outcome
   end
-end
-
-When("user adds outcomes for Crime Lower Criminal Proceedings with fields like this:") do |table|
-  sr = CWAProvider.submission.schedule_ref
-  outcome_data = table.hashes
-  @submissions_saved = outcome_data.size
-  outcome_data.each do |test_data_row|
-    submission_list_page = SubmissionListPage.new
-    submission_list_page.add_outcome_button.click
-    test_data_row['schedule_ref'] = sr
-    builder = Helpers::ScreenFieldBuilder.from(
-      area_of_law: 'crime_lower',
-      category_of_law: 'criminal_proceedings',
-      stage_reached_code: test_data_row['matter_type'])
-    builder.overrides = test_data_row
-    page = AddOutcomePage.new(builder)
-    page.add_outcome
-  end
-end
-
-When ('user adds an outcome for Immigration with {string}, {string}, {string}, {string}, {string}, {string} and {string}') \
-do |case_id, matter_type, exemption_criteria_satisfied, ecf_ref, case_start_date, pa, ap|
-  submission_list_page = SubmissionListPage.new
-  submission_list_page.add_outcome_button.click
-  values = {
-    matter_type: matter_type,
-    claim_type: 'Completed Matter Claim',
-    schedule_ref: CWAProvider.legal_help_submission.schedule_ref,
-    case_ref_number: 'TestCaseRef',
-    case_start_date: case_start_date,
-    case_id: case_id,
-    procurement_area: pa,
-    access_point: ap,
-    client_forename: 'Test',
-    client_surname: 'Person',
-    client_date_of_birth: '01-Nov-2015',
-    ucn: '01112015/T/PERS',
-    postal_appl_accp: 'N',
-    ho_ucn: 'A9999999',
-    gender: 'Male',
-    ethnicity: '00-Other',
-    disability: 'NCD-Not Considered Disabled',
-    client_post_code: 'SW1H 9AJ',
-    work_concluded_date: '01-Nov-2019',
-    advice_time: '0',
-    travel_time: '0',
-    waiting_time: '0',
-    profit_cost: '100.00',
-    disbursements_amount: '0',
-    counsel_cost: '0',
-    disbursements_vat: '0',
-    vat_indicator: 'No',
-    legacy_case: 'No',
-    ho_interview: '0',
-    ait_hearing_centre: '16-Other',
-    travel_waiting_costs: '0',
-    adjourned_hearing_fee: '0',
-    travel_costs: '0',
-    jr_form_filling: '0',
-    cmrh_oral: '0',
-    cmrh_telephone: '0',
-    substantive_hearing: 'No',
-    stage_reached: '',
-    outcome_code: 'IX',
-    exemption_criteria_satisfied: exemption_criteria_satisfied,
-    excl_case_funding_ref: ecf_ref
-  }
-  builder = Helpers::ScreenFieldBuilder.from(
-    area_of_law: 'legal_help',
-    category_of_law: 'immigration_and_asylum')
-  builder.overrides = values
-  page = AddOutcomePage.new(builder)
-  page.add_outcome
-end
-
-When ('user adds an outcome for Family with {string}, {string}, {string}, {string}, {string}, {string} and {string}') \
-do |case_id, matter_type, exemption_criteria_satisfied, ecf_ref, case_start_date, pa, ap|
-  submission_list_page = SubmissionListPage.new
-  submission_list_page.add_outcome_button.click
-  values = {
-    matter_type: matter_type,
-    claim_type: 'Completed Matter Claim',
-    schedule_ref: CWAProvider.legal_help_submission.schedule_ref,
-    case_ref_number: 'TestCaseRef',
-    case_start_date: case_start_date,
-    case_id: case_id,
-    procurement_area: pa,
-    access_point: ap,
-    client_forename: 'Test',
-    client_surname: 'Person',
-    client_date_of_birth: '01-May-1980',
-    ucn: '01051980/T/PERS',
-    postal_appl_accp: 'N',
-    gender: 'Male',
-    ethnicity: '00-Other',
-    disability: 'NCD-Not Considered Disabled',
-    client_post_code: 'SW1H 9AJ',
-    work_concluded_date: '01-Nov-2019',
-    advice_time: '0',
-    travel_time: '0',
-    waiting_time: '0',
-    profit_cost: '100.00',
-    disbursements_amount: '0',
-    counsel_cost: '0',
-    disbursements_vat: '0',
-    vat_indicator: 'No',
-    london_rate: 'No',
-    travel_waiting_costs: '0',
-    value_of_costs_damages_awarded: '0',
-    local_authority_number: '1234',
-    client_type: 'P-Parent',
-    outcome_code: 'FF-Settlement with no benefit for the client',
-    case_stage_level: 'FPL10-Div',
-    exemption_criteria_satisfied: exemption_criteria_satisfied,
-    excl_case_funding_ref: ecf_ref
-  }
-  builder = Helpers::ScreenFieldBuilder.from(
-    area_of_law: 'legal_help',
-    category_of_law: 'family')
-  builder.overrides = values
-  page = AddOutcomePage.new(builder)
-  page.add_outcome
-end
-
-When ('user adds an outcome for Medical Negligence with {string}, {string}, {string}, {string}, {string}, {string} and {string}') \
-do |case_id, matter_type, exemption_criteria_satisfied, ecf_ref, case_start_date, pa, ap|
-  submission_list_page = SubmissionListPage.new
-  submission_list_page.add_outcome_button.click
-  values = {
-    matter_type: matter_type,
-    claim_type: 'Completed Matter Claim',
-    schedule_ref: CWAProvider.legal_help_submission.schedule_ref,
-    case_ref_number: 'TestCaseRef',
-    case_start_date: case_start_date,
-    case_id: case_id,
-    procurement_area: pa,
-    access_point: ap,
-    client_forename: 'Test',
-    client_surname: 'Person',
-    client_date_of_birth: '01-May-1980',
-    ucn: '01051980/T/PERS',
-    postal_appl_accp: 'N',
-    gender: 'Male',
-    ethnicity: '00-Other',
-    disability: 'NCD-Not Considered Disabled',
-    client_post_code: 'SW1H 9AJ',
-    work_concluded_date: '01-Nov-2019',
-    advice_time: '0',
-    travel_time: '0',
-    waiting_time: '0',
-    profit_cost: '100.00',
-    disbursements_amount: '0',
-    counsel_cost: '0',
-    disbursements_vat: '0',
-    vat_indicator: 'No',
-    tolerance_indicator: 'No',
-    travel_waiting_costs: '0',
-    outcome_code: 'NA-Client receives damages',
-    exemption_criteria_satisfied: exemption_criteria_satisfied,
-    excl_case_funding_ref: ecf_ref
-  }
-  builder = Helpers::ScreenFieldBuilder.from(
-    area_of_law: 'legal_help',
-    category_of_law: 'clinical_negligence')
-  builder.overrides = values
-  page = AddOutcomePage.new(builder)
-  page.add_outcome
-end
-
-When ('user adds an outcome for Education with {string}, {string}, {string}, {string}, {string} and {string}') \
-do |matter_type, ecf_ref, case_start_date, pa, ap, case_id |
-  submission_list_page = SubmissionListPage.new
-  submission_list_page.add_outcome_button.click
-
-  values = {
-    matter_type: matter_type,
-    claim_type: 'Completed Matter Claim',
-    schedule_ref: CWAProvider.legal_help_submission.schedule_ref,
-    case_ref_number: 'TestCaseRef',
-    case_start_date: case_start_date,
-    case_id: case_id,
-    procurement_area: pa,
-    access_point: ap,
-    client_forename: 'Test',
-    client_surname: 'Person',
-    client_date_of_birth: '01-May-1980',
-    ucn: '01051980/T/PERS',
-    postal_appl_accp: 'N',
-    gender: 'Male',
-    ethnicity: '00-Other',
-    disability: 'NCD-Not Considered Disabled',
-    client_post_code: 'SW1H 9AJ',
-    work_concluded_date: '02-Sep-2019',
-    advice_time: '0',
-    travel_time: '0',
-    waiting_time: '0',
-    profit_cost: '100.00',
-    disbursements_amount: '0',
-    counsel_cost: '0',
-    disbursements_vat: '0',
-    vat_indicator: 'No',
-    tolerance_indicator: 'No',
-    travel_waiting_costs: '0',
-    stage_reached: 'EA-First meeting',
-    outcome_code: 'EA-Client receives damages',
-    exemption_criteria_satisfied: '',
-    excl_case_funding_ref: ecf_ref,
-    transfer_date: ''
-  }
-  builder = Helpers::ScreenFieldBuilder.from(
-    area_of_law: 'legal_help',
-    category_of_law: 'education')
-  builder.overrides = values
-  page = AddOutcomePage.new(builder)
-  page.add_outcome
-end
 
 Then("the outcome does not save and gives an error containing:") do |string|
   page = AddOutcomePage.new
