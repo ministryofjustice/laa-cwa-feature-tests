b387bd286f00c380a43ae4feae921da5306ced68
vimal <vimal.suresh@digital.justice.gov.uk>
2023-07-25 14:56:44 +0100

    change shell script

--- runner.sh
+++ runner.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+echo first run
 date
-bundle exec parallel_cucumber features/validations/legal_help/IAbulk -n 6 
+cucumber features/validations/legal_help/IAbulk/1.feature
+date
+echo secound run
+date
+cucumber features/validations/legal_help/IAbulk/1.feature
+date
+echo third run
+date
+cucumber features/validations/legal_help/IAbulk/1.feature
+date
+echo fourth run
+date
+cucumber features/validations/legal_help/IAbulk/1.feature
+date
+echo Fifth run
+date
+cucumber features/validations/legal_help/IAbulk/1.feature
 date
