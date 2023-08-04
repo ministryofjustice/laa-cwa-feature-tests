fcdef9ca9b950ed7db90a26ba4ba19f8bde16d55
vimal <vimal.suresh@digital.justice.gov.uk>
2023-08-04 15:43:14 +0100

    docker

--- features/step_definitions/dbconnect.rb
+++ features/step_definitions/dbconnect.rb
@@ -1,31 +1,11 @@
-# require "dbi"
+require oci8
+conn = OCI8.new('apps', 'app5cwa','host.docker.internal:1571/cwa')
+statement = " select * from xxlsc_civil_scope_rules_v where mt1 = 'IACA'"
+cursor = con.parse(statement)
 
-# begin
-#     dbConnection = DBI.connect("cwa-app1.aws.dev.legalservices.gov.uk", "apps", "app5cwa")
-#     stmt = dbConnection.prepare("SELECT * FROM CUSTOMERS")
-#     stmt.execute(100)
-#     stmt.fetch do |customer|
-#         puts "Customer ID :- #{customer[0]}"
-#         puts "Customer Name :- #{customer[1]}, #{customer[2]}"
-#         puts "Customer Age :- #{customer[3]}"
-#     end
-#     stmt.finish
-# rescue DBI::DatabaseError => de
-#     puts de.errstr
-# ensure
-#     if dbConnection
-#         dbConnection.disconnect
-#     end
-# end
-
-require:
-require 'execjs'
-require 'date'
-require 'oci8'
-require 'sequel'
-
-in the page (siteprism)  or step definitions
-def delete_Inclusao
-conn = OCI8.new('apps', 'app5cwa','localhost:1571/cwa') conn.exec('table WHERE login = :1', 'xxxx')
-conn.commit
+while r = cursor.fetch()
+puts r.join(', ')
 end
+
+cursor.close
+conn.logoff
