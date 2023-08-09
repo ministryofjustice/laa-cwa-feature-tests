c023ade4916cd6df41d6b8ddfca85748eddab467
vimal <vimal.suresh@digital.justice.gov.uk>
2023-08-09 16:33:12 +0100

    kube config

--- features/step_definitions/dbconnect.rb
+++ features/step_definitions/dbconnect.rb
@@ -1,7 +1,7 @@
 require oci8
 conn = OCI8.new('apps', 'app5cwa','host.docker.internal:1571/cwa')
 statement = " select * from xxlsc_civil_scope_rules_v where mt1 = 'IACA'"
-cursor = con.parse(statement)
+cursor = conn.parse(statement)
 
 while r = cursor.fetch()
 puts r.join(', ')
