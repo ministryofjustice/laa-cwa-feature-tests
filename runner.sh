9471d41f4598cfbedcacab7779e6e2d73c274716
vimal <vimal.suresh@digital.justice.gov.uk>
2023-08-23 23:51:40 +0100

    remove runner

--- runner.sh
+++ runner.sh
@@ -1,3 +1,5 @@
+export HEADLESS='true'
+export TEST_ENV='tst'
 cucumber  --format pretty --expand --format json -o "report.json"  features
 ruby features/support/report_builder.rb 
 open cucumber_web_report.html
