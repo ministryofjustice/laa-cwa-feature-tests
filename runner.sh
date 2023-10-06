d50b0c0b87711b24c3dbb568e41d361e2cdf863b
vimal <vimal.suresh@digital.justice.gov.uk>
2023-10-06 12:21:01 +0100

    tmp change

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ export TEST_ENV='tst'
 
 cucumber  --format pretty --expand --format json -o "report.json"
 
-bundle exec parallel_cucumber features/validations -n 2
+bundle exec parallel_cucumber features/validations -n 3
 
 # bundle exec parallel_cucumber features/validations -n 2 &
 # step1_pid=1$!
