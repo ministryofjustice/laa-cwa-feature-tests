4193c938038a6178540037b78895d1c28ce20f92
Solomon Ahmed <51327770+solomonahmed@users.noreply.github.com>
2025-04-29 17:30:01 +0100

    move ruby script from config.yml to stand alone file.  Mods to config.yml.

---
+++ docker/scripts/push_to_redis.rb
@@ -0,0 +1,16 @@
+require 'redis'
+
+RELEASE_NAME = File.read('release_name.txt').strip
+REDIS_SERVICE = "#{RELEASE_NAME}-redis-service"
+redis = Redis.new(host: REDIS_SERVICE, port: 6379)
+
+if File.exist?('features.txt') && !File.zero?('features.txt')
+  File.readlines('features.txt').each do |line|
+    redis.lpush('mylist', line.strip)
+  end
+else
+  feature_files = `find . -type f -name "*.feature"`.split("\n")
+  feature_files.each do |file|
+    redis.lpush('mylist', file)
+  end
+end
