6783745c60f7c699692a3513ef3632b151e4470f
Solomon Ahmed <51327770+solomonahmed@users.noreply.github.com>
2025-05-13 16:41:00 +0100

    improve logging and store screenshots in a pvc pod

--- features/step_definitions/cwa_login_steps.rb
+++ features/step_definitions/cwa_login_steps.rb
@@ -5,12 +5,46 @@ When('user clicks on CWA link') do
   end
 end
 
+# Then('CWA application page is displayed') do
+#   cwa_window = switch_to_window { page.title == 'Oracle Applications Home Page' }
+#   within_window(cwa_window) do
+#     expect(page).to have_content('Navigator')
+#   end
+# end
+
 Then('CWA application page is displayed') do
-  cwa_window = switch_to_window { page.title == 'Oracle Applications Home Page' }
+  cwa_window = nil
+
+  puts "Looking for window with title 'Oracle Applications Home Page'..."
+
+  # Retry logic to wait for the window to appear
+  5.times do |attempt|
+    puts "Attempt #{attempt + 1}: Checking open windows..."
+    windows.each do |w|
+      within_window(w) do
+        title = page.title
+        puts "Found window with title: '#{title}'"
+        if title == 'Oracle Applications Home Page'
+          cwa_window = w
+          break
+        end
+      end
+    end
+    break if cwa_window
+    sleep 1
+  end
+
+  unless cwa_window
+    puts "ERROR: Could not find a window with the expected title."
+    save_screenshot("/screenshots/cwa_failure_#{Time.now.to_i}.png"
+    raise Capybara::WindowError, "CWA window not found after retries"
+  end
+
   within_window(cwa_window) do
     expect(page).to have_content('Navigator')
   end
 end
+
 
 Given('user is logged into cwa') do
     case ENV['TEST_ENV']
